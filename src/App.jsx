3f767bface7a93c4ca33fa966f27f4030b5e6c5d
LucasSoares482 <lluquinhas482@gmail.com>
2024-09-09 20:06:17 -0300

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,53 @@
 // codigo basico e padrão gerado pelo propio stack blitz para React.jsx
 
-//  import { useState } from 'react';
-//  import reactLogo from './assets/react.svg';
-//  import viteLogo from '/vite.svg';
-//  import './App.css';
-
-//  function App() {
-//    const [count, setCount] = useState(0);
-
-//    return (
-//      <>
-//        <div>
-//          <a href="https:vitejs.dev" target="_blank">
-//            <img src={viteLogo} className="logo" alt="Vite logo" />
-//          </a>
-//          <a href="https:react.dev" target="_blank">
-//            <img src={reactLogo} className="logo react" alt="React logo" />
-//          </a>
-//        </div>
-//        <h1>Lucas Soares Dos Santos</h1>
-//        <div className="card">
-//          <button onClick={() => setCount((count) => count + 1)}>
-//            count is {count}
-//          </button>
-//          <p>
-//            Edit <code>src/App.jsx</code> and save to test HMR
-//          </p>
-//        </div>
-//        <p className="read-the-docs">
-//          Click on the Vite and React logos to learn more
-//        </p>
-//      </>
-//    );
-//  }
-
-//  export default App;
-
-
-// codigo para importar MeuComponentes.jsx
-
-// import React from 'react';
-// import MeuComponente from './componentes/MeuComponentes';
-// import MeuComponentes from './componentes/MeuComponentes';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <MeuComponente />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-//lista de compras exercicio 2 do pdf 2
-
-// src/App.jsx
-// import React from 'react';
-// import ListaDeCompras from './componentes/ListaDeCompras';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <ListaDeCompras />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from 'react';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import MeuComponentes from './componentes/MeuComponentes';
 import Imagem from './componentes/Imagem';
 import Textos from './componentes/Textos';
 import ListaDeCompras from './componentes/ListaDeCompras';
 import Contador from './componentes/Contador';
-function App() {
-  return (
-    <div className="App">
-      <h1>Titulo</h1>
-      <MeuComponentes />
-      <Imagem />
-      <Textos />
-      <ListaDeCompras />
-      <Contador/>
-    </div>
-  );
-}
+import ContadorNegativo from './componentes/ContadorNegativo';
+
+  function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+      <>
+      {/* codigo basico gerado pelo stackblitz */}
+        <div>
+          <a href="https:vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https:react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Lucas Soares Dos Santos</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <hr />
+          {/* componentes separados */}
+        <MeuComponentes />
+        <Imagem />
+        <Textos />
+        <ListaDeCompras />
+        <Contador/>
+        <ContadorNegativo/>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+    );
+  }
 
 export default App;
 
